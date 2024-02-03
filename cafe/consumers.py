dde539e6d2d1f01985584ecb399819f4622ad2ca
IslomjonCoder <106533499+IslomjonCoder@users.noreply.github.com>
2024-02-03 23:49:52 +0500

    chat websocket

--- cafe/consumers.py
+++ cafe/consumers.py
@@ -1,64 +1,14 @@
-# import json
-#
-# from asgiref.sync import async_to_sync
-# from channels.generic.websocket import WebsocketConsumer
-#
-# from .models import Room
-#
-#
-# class ChatConsumer(WebsocketConsumer):
-#
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(args, kwargs)
-#         self.room_name = None
-#         self.room_group_name = None
-#         self.room = None
-#
-#     def connect(self):
-#         self.room_name = self.scope['url_route']['kwargs']['room_name']
-#         self.room_group_name = f'chat_{self.room_name}'
-#         self.room = Room.objects.get(name=self.room_name)
-#
-#         # connection has to be accepted
-#         self.accept()
-#
-#         # join the room group
-#         async_to_sync(self.channel_layer.group_add)(
-#             self.room_group_name,
-#             self.channel_name,
-#         )
-#
-#     def disconnect(self, close_code):
-#         async_to_sync(self.channel_layer.group_discard)(
-#             self.room_group_name,
-#             self.channel_name,
-#         )
-#
-#     def receive(self, text_data=None, bytes_data=None):
-#
-#         text_data_json = json.loads(text_data)
-#         message = text_data_json['message']
-#
-#         # send chat message event to the room
-#         async_to_sync(self.channel_layer.group_send)(
-#             self.room_group_name,
-#             {
-#                 'type': 'chat_message',
-#                 'message': message,
-#             }
-#         )
-#
-#     def chat_message(self, event):
-#         self.send(text_data=json.dumps(event))
+import logging
 import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 import asyncio
 
-
+logger = logging.getLogger(__name__)
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         print('hello')
+        logger.info('WebSocket connected')
         self.room_name = 'test'  # self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = "chat_test"
 
