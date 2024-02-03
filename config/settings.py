061910cc0e15b32de1b40e9cef2a721986566906
IslomjonCoder <106533499+IslomjonCoder@users.noreply.github.com>
2024-02-03 20:44:22 +0500

    chat websocket

--- config/settings.py
+++ config/settings.py
@@ -87,7 +87,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [('REDIS_URL', 6379)],
+            "hosts": [('chatgram.up.railway.app', 6379)],
         },
     },
 }
