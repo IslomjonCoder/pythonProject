9f680cee7ba1fda4f57250e30a6141f35f87ca78
IslomjonCoder <106533499+IslomjonCoder@users.noreply.github.com>
2024-02-07 01:14:06 +0500

    chat websocket

--- cafe/consumers.py
+++ cafe/consumers.py
@@ -9,10 +9,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_group_name = f"chat_{self.room_name}"
         print('hello', self.room_group_name)
         # Join the room group
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
+        # await self.channel_layer.group_add(
+        #     self.room_group_name,
+        #     self.channel_name
+        # )
 
         await self.accept()
         await self.send(text_data=json.dumps({
